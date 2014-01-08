512289469e532ab93b3a18bf041480e47ee8e1b4
drygaay <drygaay@NCBIPC1140.be-md.ncbi.nlm.nih.gov>
2014-01-08 10:52:26 -0500

    conversion of (word) text to the new text

---
+++ src/substitution/TextRule.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package substitution;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * substitution rule for the text
+ * example: "h" to "hr" as a separate word
+ * @author drygaay
+ */
+public class TextRule extends SubstitutionRule {
+    String from;
+    String to;
+    Pattern pattern;
+    
+    public TextRule(String from, String to) {
+        this.pattern = Pattern.compile("\\b" + from + "\\b");
+        this.from = from;
+        this.to = to;
+    }
+    
+       
+    @Override
+    public void substitute(String original) { 
+        Matcher matcher = pattern.matcher(original);
+        int start = 0; /// start of the string without the match
+        while (matcher.find()) {
+            String before = original.substring(start, matcher.start());
+            if ( ! before.isEmpty()) {
+                text.add(new Substitution(before, before));
+            }
+            text.add(new Substitution(from, to));
+            start = matcher.end();          
+        }
+        text.add(new Substitution(original.substring(start), original.substring(start)));
+    }
+ }
